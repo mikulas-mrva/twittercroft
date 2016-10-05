1760f7f163597a3b4a8cb7cc49cb4b924c2d6282
Mikulas Mrva <mikulas.mrva@gmail.com>
2016-10-05 01:21:07 +0100

    added an exception block and improved text output for import_countries command

--- tweetmap/management/commands/import_countries.py
+++ tweetmap/management/commands/import_countries.py
@@ -3,7 +3,6 @@ from tweetmap.models import Country
 import csv
 
 
-# todo add exceptions
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('filename', type=str)
@@ -11,20 +10,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(options['filename'])
 
-        with open(options['filename'], 'r') as country_data:
-            rows = csv.reader(country_data, delimiter=",", quotechar='"')
+        try:
+            with open(options['filename'], 'r') as country_data:
+                rows = csv.reader(country_data, delimiter=",", quotechar='"')
 
-            for row in rows:
-                if rows.line_num == 1:
-                    continue
+                for row in rows:
+                    if rows.line_num == 1:
+                        continue
 
-                _, created = Country.objects.get_or_create(
-                    name=row[0],
-                    code=row[1],
-                    lng=row[2],
-                    lat=row[3],
-                )
+                    _, created = Country.objects.get_or_create(
+                        name=row[0],
+                        code=row[1],
+                        lng=row[2],
+                        lat=row[3],
+                    )
 
-                if created:
-                    self.stdout.write('Successfully added {}.'.format(row[0]))
-        self.stdout.write('Import complete.')
+                    if created:
+                        self.stdout.write('Successfully added {}.'.format(row[0]))
+        except FileNotFoundError:
+            self.stderr.write('File does not exist: {}'.format(options['filename']))
+        else:
+            self.stdout.write(self.style.SUCCESS('Import complete.'))
