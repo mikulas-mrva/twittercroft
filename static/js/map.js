11941e5b00e8d65318806e925a2893257a3e6fd6
Mikulas Mrva <mikulas.mrva@gmail.com>
2016-10-06 17:12:05 +0100

    basic graph works

--- static/js/map.js
+++ static/js/map.js
@@ -7,14 +7,14 @@ function unpack(rows, key) {
 var data = [{
     type: 'choropleth',
     locationmode: 'country names',
-    locations: unpack(rows, 'location'),
+    locations: unpack(rows, 'country'),
     z: unpack(rows, 'number_of_mentions'),
-    text: unpack(rows, 'location'),
+    text: unpack(rows, 'country'),
     autocolorscale: true
 }];
 
 var layout = {
-  title: 'Countries mentioned in the tweets of Verisk Maplecrofts',
+  title: 'Countries mentioned in the tweets of Verisk Maplecroft',
   geo: {
       projection: {
           type: 'robinson'
@@ -23,3 +23,14 @@ var layout = {
 };
 
 Plotly.plot(mapDiv, data, layout, {showLink: false});
+
+myPlot.on('plotly_hover', function(data){
+    var infotext = data.points.map(function(d){
+      return (d.data.name+': x= '+d.x+', y= '+d.y.toPrecision(3));
+    });
+
+    hoverInfo.innerHTML = infotext.join('');
+})
+ .on('plotly_unhover', function(data){
+    hoverInfo.innerHTML = '';
+});
